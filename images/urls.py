0ff621f37f269d3082703928c15e566d513623cb
Valerii Bondariev <valerii.bondariev@student.tuke.sk>
2024-03-18 17:42:56 +0100

    email verif added

--- images/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UploadView, DeleteView
-
-urlpatterns = [
-    path('upload/', UploadView.as_view(), name='image_upload'),
-    path('delete/<int:pk>', DeleteView.as_view(), name='image_del'),
-]
